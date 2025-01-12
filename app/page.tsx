82650fdbacbcd0d9630bb160f63dd856210b6653
Remiiiiii <tuf09783@temple.edu>
2025-01-12 13:29:53 -0500

    implemented an OTP feature in the Home component for admin access to admin dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import PatientForm from "@/components/forms/PatientForm";
-import { Link } from "lucide-react";
+import { PasskeyModal } from "@/components/PasskeyModal";
+import Link from "next/link";
 import Image from "next/image";
 
-export default function Home() {
+const Home = async ({ searchParams }: SearchParamProps) => {
+  const params = await searchParams;
+  const isAdmin = params?.admin === "true";
   return (
     <div className="flex h-screen max-h-screen">
-      {/* TODO: OTP Verification | Passkey Modal*/}
+      {isAdmin && <PasskeyModal />}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -13,7 +16,7 @@ export default function Home() {
             height={1000}
             width={1000}
             alt="patient"
-            className="mb-12 h-[150px] w-[250px]"
+            className="mb-12 h-[150px] w-[250px] ml-[-25px]"
           />
           <PatientForm />
           <div className="text-14-regular mt-20 flex justify-between">
@@ -27,7 +30,7 @@ export default function Home() {
         </div>
       </section>
       <Image
-        src="/assets/images/onboarding-img.png"
+        src="/assets/images/landing-dr-img.png"
         height={1000}
         width={1000}
         alt="patient"
@@ -35,4 +38,6 @@ export default function Home() {
       />
     </div>
   );
-}
+};
+
+export default Home;
