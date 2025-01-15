8061b0d787557eb5888fd60081d6818a6fb2b080
Remiiiiii <tuf09783@temple.edu>
2025-01-15 15:56:45 -0500

    previous commit did not resolve the issue. Removed use client from the component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-"use client";
-
-import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import { PatientForm } from "@/components/forms/PatientForm";
 import { PasskeyModal } from "@/components/PasskeyModal";
@@ -12,38 +9,36 @@ const Home = () => {
   const isAdmin = searchParams?.get("admin") === "true";
 
   return (
-    <Suspense>
-      <div className="flex h-screen max-h-screens">
-        {isAdmin && <PasskeyModal />}
-        <section className="remove-scrollbar container my-auto">
-          <div className="sub-container max-w-[496px]">
-            <Image
-              src="/assets/icons/logo-ks.png"
-              height={1000}
-              width={1000}
-              alt="patient"
-              className="mb-12 h-[50px] w-[200px] ml-[-5px]"
-            />
-            <PatientForm />
-            <div className="text-14-regular mt-20 flex justify-between">
-              <p className="justify-items-end text-dark-600 xl:text-left">
-                © 2025 KareSphere
-              </p>
-              <Link href="/?admin=true" className="text-green-500">
-                Admin
-              </Link>
-            </div>
+    <div className="flex h-screen max-h-screens">
+      {isAdmin && <PasskeyModal />}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-ks.png"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-[50px] w-[200px] ml-[-5px]"
+          />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2025 KareSphere
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
           </div>
-        </section>
-        <Image
-          src="/assets/images/landing-dr-img.png"
-          height={1000}
-          width={1000}
-          alt="patient"
-          className="hidden object-contain h-50 w-50 md:block max-w-[50%] mt-[10%]"
-        />
-      </div>
-    </Suspense>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/landing-dr-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="hidden object-contain h-50 w-50 md:block max-w-[50%] mt-[10%]"
+      />
+    </div>
   );
 };
 
