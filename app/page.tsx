297358686493bfe12bd92859cc263f9b8156c37c
Remiiiiii <tuf09783@temple.edu>
2025-01-15 15:48:52 -0500

    Error during deployment to Vercel. UseSearchParams() should  be wrapped in a suspense boundry. Wrapped the Home component tsx in a suspense component from react

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import { PatientForm } from "@/components/forms/PatientForm";
 import { PasskeyModal } from "@/components/PasskeyModal";
@@ -11,36 +12,38 @@ const Home = () => {
   const isAdmin = searchParams?.get("admin") === "true";
 
   return (
-    <div className="flex h-screen max-h-screens">
-      {isAdmin && <PasskeyModal />}
-      <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[496px]">
-          <Image
-            src="/assets/icons/logo-ks.png"
-            height={1000}
-            width={1000}
-            alt="patient"
-            className="mb-12 h-[50px] w-[200px] ml-[-5px]"
-          />
-          <PatientForm />
-          <div className="text-14-regular mt-20 flex justify-between">
-            <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2025 KareSphere
-            </p>
-            <Link href="/?admin=true" className="text-green-500">
-              Admin
-            </Link>
+    <Suspense>
+      <div className="flex h-screen max-h-screens">
+        {isAdmin && <PasskeyModal />}
+        <section className="remove-scrollbar container my-auto">
+          <div className="sub-container max-w-[496px]">
+            <Image
+              src="/assets/icons/logo-ks.png"
+              height={1000}
+              width={1000}
+              alt="patient"
+              className="mb-12 h-[50px] w-[200px] ml-[-5px]"
+            />
+            <PatientForm />
+            <div className="text-14-regular mt-20 flex justify-between">
+              <p className="justify-items-end text-dark-600 xl:text-left">
+                © 2025 KareSphere
+              </p>
+              <Link href="/?admin=true" className="text-green-500">
+                Admin
+              </Link>
+            </div>
           </div>
-        </div>
-      </section>
-      <Image
-        src="/assets/images/landing-dr-img.png"
-        height={1000}
-        width={1000}
-        alt="patient"
-        className="hidden object-contain h-50 w-50 md:block max-w-[50%] mt-[10%]"
-      />
-    </div>
+        </section>
+        <Image
+          src="/assets/images/landing-dr-img.png"
+          height={1000}
+          width={1000}
+          alt="patient"
+          className="hidden object-contain h-50 w-50 md:block max-w-[50%] mt-[10%]"
+        />
+      </div>
+    </Suspense>
   );
 };
 
